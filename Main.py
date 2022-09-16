07ad24f7eda13156762d4294e336d3bc43c9affd
zachm7115 <39175068+zachm7115@users.noreply.github.com>
2022-09-16 17:27:56 -0400

    Created main python files

---
+++ Main.py
@@ -0,0 +1,17 @@
+import gamePhysics
+import vision
+
+def main():
+    #initialize vision
+
+    #initialize game
+
+    #Run vision until capture taken
+
+    #Run game until guy dies or quit key pressed
+
+    #Either back to vision or quit
+    print("finished")
+
+
+main()
