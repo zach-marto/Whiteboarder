18ec309612036d2c87c87b6588c46bb2d2d0f66a
zachm7115 <39175068+zachm7115@users.noreply.github.com>
2022-09-23 16:08:24 -0400

    swapped function calls from gamePhysics to vision

--- Main.py
+++ Main.py
@@ -11,10 +11,10 @@ import time
 
 def main():
     #initialize vision and screen
-    camera = gamePhysics.initCamera()
-    width = gamePhysics.getWidth(camera)
-    height = gamePhysics.getHeight(camera)
-    screen = gamePhysics.initScreen(width, height)
+    camera = vision.initCamera()
+    width = vision.getWidth(camera)
+    height = vision.getHeight(camera)
+    screen = vision.initScreen(width, height)
     #This is the array of contours that will be used in-game
     contours = []
 
@@ -23,7 +23,7 @@ def main():
     #Run vision until capture taken or program quit
     runGame = False
     while not runGame:
-        runGame, contours = gamePhysics.visionStep(screen, camera, contours)
+        runGame, contours = vision.visionStep(screen, camera, contours)
     
     #Run game until guy dies or quit key pressed
 
